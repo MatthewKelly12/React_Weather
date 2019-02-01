f3bc6ca5b5e9a9db9ec4dc5f503d7a529f169038
Matthew Kelly <mkonabike@gmail.com>
2019-01-31 18:14:21 -0600

    created weather component

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class Weather extends Component {
+	state = {
+		people: []
+	}
+	componentDidMount() {
+		return axios.get("https://swapi.co/api/people")
+		.then(r => {
+		  console.log(r.data.results)
+		  this.setState({people: r.data.results})
+		})
+	}
 
 	render () {
-		return (
-			<div>Weather</div>
-		)
+			const people = this.state.people
+			console.log(people)
+			return (
+				<div>
+					{people.map(p => {
+
+						return(
+							<div>
+								<h1>{p.name}</h1>
+							</div>
+						)})}
+				</div>
+			)
+
 	}
 }
 
