43b586edff00cea14105979c2ceb80d988061439
Matthew Kelly <mkonabike@gmail.com>
2019-02-01 11:47:32 -0600

    moved state and componentDidMount to top level in App, passed weather down via props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import './App.css';
 import Weather from './Weather';
 
 class App extends Component {
+
+	state = {
+		weather: {}
+	}
+	componentDidMount() {
+		return axios.get('http://api.openweathermap.org/data/2.5/weather?q=Nashville&units=imperial&APPID=25e989bd41e3e24ce13173d8126e0fd6')
+		.then(response => {
+			// CREATE OBJECT TO HOLD WEATHER DATA
+			const weatherData = {
+				'city': response.data.name,
+				'temp': response.data.main.temp,
+				'humidity':	response.data.main.humidity,
+				'wind': response.data.wind.speed,
+				'description':	response.data.weather[0].description,
+				'icon':response.data.weather[0].icon
+			}
+			console.log(response)
+
+		  	this.setState({weather: weatherData})
+			}
+		)
+	}
   render() {
     return (
       <div className="App">
-			<Weather/>
+			<Weather weather={this.state.weather}/>
       </div>
     );
   }
--- src/Weather.js
+++ src/Weather.js
@@ -1,32 +1,10 @@
 import React, { Component } from 'react';
-import axios from 'axios'
+
 
 class Weather extends Component {
 
-	state = {
-		weather: {}
-	}
-	componentDidMount() {
-		return axios.get('http://api.openweathermap.org/data/2.5/weather?q=Nashville&units=imperial&APPID=25e989bd41e3e24ce13173d8126e0fd6')
-		.then(response => {
-			// CREATE OBJECT TO HOLD WEATHER DATA
-			const weatherData = {
-				'city': response.data.name,
-				'temp': response.data.main.temp,
-				'humidity':	response.data.main.humidity,
-				'wind': response.data.wind.speed,
-				'description':	response.data.weather[0].description,
-				'icon':response.data.weather[0].icon
-			}
-			console.log(response)
-
-		  	this.setState({weather: weatherData})
-			}
-		)
-	}
-
 	render () {
-			const weather = this.state.weather
+			const weather = this.props.weather
 			const pic = `http://openweathermap.org/img/w/${weather.icon}.png`
 			return (
 				<div>
